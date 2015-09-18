5659f7ed9b563d0b552bca3062b1d18c7a433628
Cole Campbell <colemcampbell>
2015-09-18 16:11:54 -0500

    Add twitter scraping to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require "twitter"
+
+TWITTER_HANDLES = {
+	sanders: "@SenSanders",
+	clinton: "@HillaryClinton",
+	chafee: "@LincolnChafee",
+	omalley: "@MartinOMalley",
+	webb: "@JimWebbUSA",
+	bush: "@JebBush",
+	carson: "@RealBenCarson",
+	christie: "@GovChristie",
+	cruz: "@SenTedCruz",
+	fiorina: "@CarlyFiorina",
+	gilmore: "@gov_gilmore",
+	graham: "@LindseyGrahamSC",
+	huckabee: "@GovMikeHuckabee",
+	jindal: "@BobbyJindal",
+	kasich: "@JohnKasich",
+	pataki: "@GovernorPataki",
+	paul: "@RandPaul",
+	rubio: "@marcorubio",
+	santorum: "@RickSantorum",
+	trump: "@realDonaldTrump",
+	walker: "@ScottWalker"
+}
+
+
+client = Twitter::REST::Client.new do |config|
+	config.consumer_key			= Rails.application.secrets.twitter_consumer_key
+	config.consumer_secret	= Rails.application.secrets.twitter_consumer_secret
+end
+
+raw_text = {}
+
+TWITTER_HANDLES.each do |candidate, handle|
+	raw_text[candidate] = client.user_timeline(handle, count: 200, include_rts: false).map { |tweet| tweet.text }
+end
+
+raw_text.each { |candidate, tweets| p tweets.count }
