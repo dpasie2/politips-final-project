2950c3c8b359569e29755ad48757dd2e2e1fde54
alita42 <alita42@gmail.com>
2015-09-20 13:42:45 -0500

    adding category and keyword seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,45 +6,45 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# require "twitter"
-#
-# TWITTER_HANDLES = {
-# 	sanders: "@SenSanders",
-# 	clinton: "@HillaryClinton",
-# 	chafee: "@LincolnChafee",
-# 	omalley: "@MartinOMalley",
-# 	webb: "@JimWebbUSA",
-# 	bush: "@JebBush",
-# 	carson: "@RealBenCarson",
-# 	christie: "@GovChristie",
-# 	cruz: "@SenTedCruz",
-# 	fiorina: "@CarlyFiorina",
-# 	gilmore: "@gov_gilmore",
-# 	graham: "@LindseyGrahamSC",
-# 	huckabee: "@GovMikeHuckabee",
-# 	jindal: "@BobbyJindal",
-# 	kasich: "@JohnKasich",
-# 	pataki: "@GovernorPataki",
-# 	paul: "@RandPaul",
-# 	rubio: "@marcorubio",
-# 	santorum: "@RickSantorum",
-# 	trump: "@realDonaldTrump",
-# 	walker: "@ScottWalker"
-# }
-#
-#
-# client = Twitter::REST::Client.new do |config|
-# 	config.consumer_key			= Rails.application.secrets.twitter_consumer_key
-# 	config.consumer_secret	= Rails.application.secrets.twitter_consumer_secret
-# end
-#
-# raw_text = {}
-#
-# TWITTER_HANDLES.each do |candidate, handle|
-# 	raw_text[candidate] = client.user_timeline(handle, count: 200, include_rts: false).map { |tweet| tweet.text }
-# end
-#
-# raw_text.each { |candidate, tweets| p tweets.count }
+require "twitter"
+
+TWITTER_HANDLES = {
+	sanders: "@SenSanders",
+	clinton: "@HillaryClinton",
+	chafee: "@LincolnChafee",
+	omalley: "@MartinOMalley",
+	webb: "@JimWebbUSA",
+	bush: "@JebBush",
+	carson: "@RealBenCarson",
+	christie: "@GovChristie",
+	cruz: "@SenTedCruz",
+	fiorina: "@CarlyFiorina",
+	gilmore: "@gov_gilmore",
+	graham: "@LindseyGrahamSC",
+	huckabee: "@GovMikeHuckabee",
+	jindal: "@BobbyJindal",
+	kasich: "@JohnKasich",
+	pataki: "@GovernorPataki",
+	paul: "@RandPaul",
+	rubio: "@marcorubio",
+	santorum: "@RickSantorum",
+	trump: "@realDonaldTrump",
+	walker: "@ScottWalker"
+}
+
+
+client = Twitter::REST::Client.new do |config|
+	config.consumer_key			= Rails.application.secrets.twitter_consumer_key
+	config.consumer_secret	= Rails.application.secrets.twitter_consumer_secret
+end
+
+raw_text = {}
+
+TWITTER_HANDLES.each do |candidate, handle|
+	raw_text[candidate] = client.user_timeline(handle, count: 200, include_rts: false).map { |tweet| tweet.text }
+end
+
+raw_text.each { |candidate, tweets| p tweets.count }
 
 
 unemployment = Category.create!(name: "Unemployment")
@@ -59,79 +59,79 @@ education = Category.create!(name: "Education")
 race = Category.create!(name: "Race Relations")
 marriage = Category.create!(name: "Marriage Equality")
 
-unemployment.keywords << Keyword.create!(name: "unemployment")
-unemployment.keywords << Keyword.create!(name: "reform")
-unemployment.keywords << Keyword.create!(name: "welfare")
-unemployment.keywords << Keyword.create!(name: "homelessness")
-unemployment.keywords << Keyword.create!(name: "poverty")
-unemployment.keywords << Keyword.create!(name: "hunger")
-unemployment.keywords << Keyword.create!(name: "labor")
-unemployment.keywords << Keyword.create!(name: "union")
+unemployment.keywords << Keyword.create!(keyword: "unemployment")
+unemployment.keywords << Keyword.create!(keyword: "reform")
+unemployment.keywords << Keyword.create!(keyword: "welfare")
+unemployment.keywords << Keyword.create!(keyword: "homelessness")
+unemployment.keywords << Keyword.create!(keyword: "poverty")
+unemployment.keywords << Keyword.create!(keyword: "hunger")
+unemployment.keywords << Keyword.create!(keyword: "labor")
+unemployment.keywords << Keyword.create!(keyword: "union")
 
-taxes.keywords << Keyword.create!(name: "tax")
-taxes.keywords << Keyword.create!(name: "refund")
-taxes.keywords << Keyword.create!(name: "wealth")
-taxes.keywords << Keyword.create!(name: "inheritance")
-taxes.keywords << Keyword.create!(name: "cuts")
-taxes.keywords << Keyword.create!(name: "interest")
-taxes.keywords << Keyword.create!(name: "IRS")
-taxes.keywords << Keyword.create!(name: "reform")
+taxes.keywords << Keyword.create!(keyword: "tax")
+taxes.keywords << Keyword.create!(keyword: "refund")
+taxes.keywords << Keyword.create!(keyword: "wealth")
+taxes.keywords << Keyword.create!(keyword: "inheritance")
+taxes.keywords << Keyword.create!(keyword: "cuts")
+taxes.keywords << Keyword.create!(keyword: "interest")
+taxes.keywords << Keyword.create!(keyword: "IRS")
+taxes.keywords << Keyword.create!(keyword: "reform")
 
-health.keywords << Keyword.create!(name: "obamacare")
-health.keywords << Keyword.create!(name: "medicare")
-health.keywords << Keyword.create!(name: "medicaid")
-health.keywords << Keyword.create!(name: "insurance")
-health.keywords << Keyword.create!(name: "abortion")
+health.keywords << Keyword.create!(keyword: "obamacare")
+health.keywords << Keyword.create!(keyword: "medicare")
+health.keywords << Keyword.create!(keyword: "medicaid")
+health.keywords << Keyword.create!(keyword: "insurance")
+health.keywords << Keyword.create!(keyword: "abortion")
 
-corp.keywords << Keyword.create!(name: "corporate")
-corp.keywords << Keyword.create!(name: "curruption")
-corp.keywords << Keyword.create!(name: "lobbying")
-corp.keywords << Keyword.create!(name: "inequality")
-corp.keywords << Keyword.create!(name: "wages")
+corp.keywords << Keyword.create!(keyword: "corporate")
+corp.keywords << Keyword.create!(keyword: "curruption")
+corp.keywords << Keyword.create!(keyword: "lobbying")
+corp.keywords << Keyword.create!(keyword: "inequality")
+corp.keywords << Keyword.create!(keyword: "wages")
 
-terrorism.keywords << Keyword.create!(name: "terrorism")
-terrorism.keywords << Keyword.create!(name: "isis")
-terrorism.keywords << Keyword.create!(name: "bengazi")
-terrorism.keywords << Keyword.create!(name: "iran")
-terrorism.keywords << Keyword.create!(name: "cyberterrorism")
-terrorism.keywords << Keyword.create!(name: "islam")
+terrorism.keywords << Keyword.create!(keyword: "terrorism")
+terrorism.keywords << Keyword.create!(keyword: "isis")
+terrorism.keywords << Keyword.create!(keyword: "bengazi")
+terrorism.keywords << Keyword.create!(keyword: "iran")
+terrorism.keywords << Keyword.create!(keyword: "cyberterrorism")
+terrorism.keywords << Keyword.create!(keyword: "islam")
 
-foreign.keywords << Keyword.create!(name: "foreign")
-foreign.keywords << Keyword.create!(name: "china")
-foreign.keywords << Keyword.create!(name: "cuba")
-foreign.keywords << Keyword.create!(name: "trade")
-foreign.keywords << Keyword.create!(name: "afghanistan")
+foreign.keywords << Keyword.create!(keyword: "foreign")
+foreign.keywords << Keyword.create!(keyword: "china")
+foreign.keywords << Keyword.create!(keyword: "cuba")
+foreign.keywords << Keyword.create!(keyword: "trade")
+foreign.keywords << Keyword.create!(keyword: "afghanistan")
 
-immigration.keywords << Keyword.create!(name: "immigration")
-immigration.keywords << Keyword.create!(name: "reform")
-immigration.keywords << Keyword.create!(name: "undocumented")
-immigration.keywords << Keyword.create!(name: "illegal")
+immigration.keywords << Keyword.create!(keyword: "immigration")
+immigration.keywords << Keyword.create!(keyword: "reform")
+immigration.keywords << Keyword.create!(keyword: "undocumented")
+immigration.keywords << Keyword.create!(keyword: "illegal")
 
-climate.keywords << Keyword.create!(name: "climate")
-climate.keywords << Keyword.create!(name: "energy")
-climate.keywords << Keyword.create!(name: "keystone")
-climate.keywords << Keyword.create!(name: "green")
+climate.keywords << Keyword.create!(keyword: "climate")
+climate.keywords << Keyword.create!(keyword: "energy")
+climate.keywords << Keyword.create!(keyword: "keystone")
+climate.keywords << Keyword.create!(keyword: "green")
 
-education.keywords << Keyword.create!(name: "education")
-education.keywords << Keyword.create!(name: "testing")
-education.keywords << Keyword.create!(name: "stem")
-education.keywords << Keyword.create!(name: "technology")
-education.keywords << Keyword.create!(name: "reform")
-education.keywords << Keyword.create!(name: "vouchers")
+education.keywords << Keyword.create!(keyword: "education")
+education.keywords << Keyword.create!(keyword: "testing")
+education.keywords << Keyword.create!(keyword: "stem")
+education.keywords << Keyword.create!(keyword: "technology")
+education.keywords << Keyword.create!(keyword: "reform")
+education.keywords << Keyword.create!(keyword: "vouchers")
 
-race.keywords << Keyword.create!(name: "race")
-race.keywords << Keyword.create!(name: "ferguson")
-race.keywords << Keyword.create!(name: "racism")
-race.keywords << Keyword.create!(name: "incarceration")
-race.keywords << Keyword.create!(name: "diversity")
-race.keywords << Keyword.create!(name: "black")
-race.keywords << Keyword.create!(name: "hispanic")
+race.keywords << Keyword.create!(keyword: "race")
+race.keywords << Keyword.create!(keyword: "ferguson")
+race.keywords << Keyword.create!(keyword: "racism")
+race.keywords << Keyword.create!(keyword: "incarceration")
+race.keywords << Keyword.create!(keyword: "diversity")
+race.keywords << Keyword.create!(keyword: "black")
+race.keywords << Keyword.create!(keyword: "hispanic")
 
-marriage.keywords << Keyword.create!(name: "marriage")
-marriage.keywords << Keyword.create!(name: "equality")
-marriage.keywords << Keyword.create!(name: "homosexual")
-marriage.keywords << Keyword.create!(name: "gay")
-marriage.keywords << Keyword.create!(name: "transgender")
-marriage.keywords << Keyword.create!(name: "lesbian")
-marriage.keywords << Keyword.create!(name: "legalization")
-marriage.keywords << Keyword.create!(name: "tradition")
+marriage.keywords << Keyword.create!(keyword: "marriage")
+marriage.keywords << Keyword.create!(keyword: "equality")
+marriage.keywords << Keyword.create!(keyword: "homosexual")
+marriage.keywords << Keyword.create!(keyword: "gay")
+marriage.keywords << Keyword.create!(keyword: "transgender")
+marriage.keywords << Keyword.create!(keyword: "lesbian")
+marriage.keywords << Keyword.create!(keyword: "legalization")
+marriage.keywords << Keyword.create!(keyword: "tradition")
