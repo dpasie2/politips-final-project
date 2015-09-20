9a261fcf35f77a50d701c3a0dfea9bca1782c00b
Philip Sugg <pbensugg@gmail.com>
2015-09-20 13:05:46 -0500

    add association to category

--- db/migrate/20150920174933_create_keywords.rb
+++ db/migrate/20150920174933_create_keywords.rb
@@ -3,7 +3,6 @@ class CreateKeywords < ActiveRecord::Migration
     create_table :keywords do |t|
       t.string :keyword
       t.integer :category_id
-
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150920174933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "candidates", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "party"
+    t.string   "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "keywords", force: :cascade do |t|
+    t.string   "keyword"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "scorings", force: :cascade do |t|
+    t.string   "score"
+    t.integer  "candidate_id"
+    t.integer  "category_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
