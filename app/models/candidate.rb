e4937e09b2e101367e1c9daeca93e85d95e03355
Cole Campbell <colemcampbell>
2015-09-22 18:39:59 -0500

    Change categories data to percentages

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -5,4 +5,19 @@ class Candidate < ActiveRecord::Base
   def self.get_party_members(party)
    Candidate.where(party: party)
   end
+
+  def search_tweets_for(category)
+
+  end
+
+  def categories_data
+    sum = 0
+    self.scorings.each { |scoring| sum += scoring.score }
+    self.scorings.map do |scoring|
+      value = (scoring.score.to_f / sum.to_f * 100).round
+      value = 1 if value == 0
+      { "issue" => scoring.category.name.downcase, "value" => (scoring.score.to_f / sum.to_f * 100).round }
+    end
+  end
+
 end
