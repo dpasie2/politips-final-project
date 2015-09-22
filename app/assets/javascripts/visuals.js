9fdec63e24f2d9f6565300e053984da424ec045c
Cole Campbell <colemcampbell>
2015-09-22 09:32:19 -0500

    Clean up code

--- app/assets/javascripts/visuals.js
+++ app/assets/javascripts/visuals.js
@@ -3,31 +3,32 @@ var pack, canvas;
 
 function displayChart(data, candidateName) {
   candidateName = candidateName || DEFAULT_CANDIDATE_NAME;
+  console.log(candidateName);
   var nodes = pack.nodes(data[candidateName.toLowerCase()]);
 
   // binds data to the canvas
   var node = canvas.selectAll(".node")
-    .data(nodes)
-    .enter()
-    .append("g")
+      .data(nodes)
+      .enter()
+      .append("g")
       .attr("class", "node")
       .attr("transform", function (d) {
-            return "translate(" + d.x + "," + d.y + ")";
+        return "translate(" + d.x + "," + d.y + ")";
       });
 
   node.append("circle")
-    .attr("fill", function (d) { return d.children ? "#fff" : "steelblue"; })
-    .attr("stroke", function (d) { return d.children ? "#fff" : "#ADADAD"; })
-    .attr("r", function (d) { return d.value/2;})
-    .transition().duration(2000)
-    .attr("r", function (d) { return d.r; })
-    .attr("opacity", .30)
-    .attr("stroke-width", "2");
+      .attr("fill", function(d) { return d.children ? "#fff" : "steelblue"; })
+      .attr("stroke", function(d) { return d.children ? "#fff" : "#ADADAD"; })
+      .attr("r", function(d) { return d.value/2;})
+      .transition().duration(2000)
+      .attr("r", function(d) { return d.r; })
+      .attr("opacity", .30)
+      .attr("stroke-width", "2");
 
   node.append("text")
-    .text(function (d) {
-      return d.children ? "" : d.issue;
-    });
+      .text(function(d) {
+        return d.children ? "" : d.issue;
+      });
 }
 
 
@@ -36,15 +37,15 @@ $(document).ready(function() {
 
   // sets width and height of the canvas
   canvas = d3.select(".d3").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-      .attr("transform", "translate(50, 50)");
+           .attr("width", width)
+           .attr("height", height)
+           .append("g")
+           .attr("transform", "translate(50, 50)");
 
   // sets the layout to pack
   pack = d3.layout.pack()
-    .size([width, height - 50])
-    .padding(10);
+         .size([width, height - 50])
+         .padding(10);
 
   // passes json as data
 
@@ -63,6 +64,4 @@ $(document).ready(function() {
       displayChart(data, this.id)
     });
   });
-
-
 });
