f1ceaf534f81f656469441449e0bcb41b9317946
Philip Sugg <pbensugg@gmail.com>
2015-09-24 18:36:33 -0500

    Fix secret key access

--- app/services/twitter_api.rb
+++ app/services/twitter_api.rb
@@ -2,8 +2,8 @@ module TwitterAPI
 
 	def self.client
 		Twitter::REST::Client.new do |config|
-    	config.consumer_key			= Rails.application.secrets.twitter_consumer_key
-    	config.consumer_secret	= Rails.application.secrets.twitter_consumer_secret
+    	config.consumer_key			= ENV["TWITTER_CONSUMER_KEY"]
+    	config.consumer_secret	= ENV["TWITTER_CONSUMER_SECRET"]
     end
 	end
 
